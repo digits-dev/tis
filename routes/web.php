cdf239b5faa868560e55eda12a29a67f84ee1045
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-12-05 16:48:44 +0800

    add dashboard index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminSalesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/sa', function () {
+    return view('sales');
+});
+
+Route::group(['middleware' => ['web']], function() {
+    Route::get(config('crudbooster.ADMIN_PATH'),[AdminSalesController::class, 'getIndex']);
 });
